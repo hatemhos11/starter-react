e792c5d4212ab844b61f4380bd5af3be55ee9847
hatemhos11 <hatemhosni00@gmail.com>
2025-02-01 18:37:34 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {},
+    },
+    plugins: [],
+};
